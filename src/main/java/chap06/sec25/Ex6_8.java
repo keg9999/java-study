4f6cc1a964f37d36876bd9306864da8c697a657f
Dawon <bdw7475@gmail.com>
2025-01-06 19:05:19 +0900

    6-25. 참조형 반환타입

--- src/main/java/chap06/sec25/Ex6_8.java
+++ src/main/java/chap06/sec25/Ex6_8.java
@@ -6,8 +6,8 @@ public class Ex6_8 {
         d.x = 10;
 
         Data3 d2 = copy(d);
-        System.out.println("d.x = " + d.x);
-        System.out.println("d2.x = " + d2.x);
+        System.out.println("d.x = " + d.x); //d.x = 10
+        System.out.println("d2.x = " + d2.x); //d2.x = 10
     }
 
     static Data3 copy(Data3 d){
@@ -18,7 +18,7 @@ public class Ex6_8 {
 
         /*
         tmp는 스택 메모리에서 제거되지만, 반환된 객체는 여전히 d2가 참조하고 있으므로 힙 메모리에 남아 있음.
-        더이상 참조하는 변수가 없을 떄 가비지 콜렉터가 힙 메모리에서 tmp도 제거한다.
+        더이상 참조하는 변수가 없을 떄 가비지 콜렉터가 tmp를 힙 메모리에서 제거한다.
          */
     }
 
