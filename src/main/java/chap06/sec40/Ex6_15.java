f62526c1ddf010b146dcb3fdbbfadbb6b83ff270
Dawon <bdw7475@gmail.com>
2025-01-27 21:41:00 +0900

    6-41. 멤버변수 초기화 예제

---
+++ src/main/java/chap06/sec40/Ex6_15.java
@@ -0,0 +1,19 @@
+package chap06.sec40;
+
+public class Ex6_15 {
+    static int[] arr = new int[10];
+
+    /** 클래스 초기화 블럭 */
+    static {
+        for(int i=0;i<arr.length;i++){
+            //1과 10 사이의 임의의 값을 배열 arr에 저장한다.
+            arr[i] = (int)(Math.random()*10) + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        for(int i=0; i<arr.length;i++){
+            System.out.println("arr[" + i + "] :" + arr[i]);
+        }
+    }
+}
